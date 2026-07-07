44dd9feb4e5d9c0f9146c89253f3f10d8a284f29
bhoomiy <bhoomiyadav039@gmail.com>
2026-07-07 19:24:37 +0530

    completed with flappy bird game using dqn

--- reinforcement_learning/flappy_bird_game/experience_replay.py
+++ reinforcement_learning/flappy_bird_game/experience_replay.py
@@ -4,7 +4,7 @@ import random
 class ReplayMemory():
     #create FIFO queue-experience replay
     def __init__(self,maxlen,seed=None): #maxlen-->how much experiences we will store
-        self.memoery=deque([],maxlen=maxlen)
+        self.memory=deque([],maxlen=maxlen)
 
     #append experiences into the memory
     def append(self,new_exp):
@@ -12,8 +12,8 @@ class ReplayMemory():
 
     #choose random experiences of a particular length(sample_size) for training
     def sample(self,sample_size):
-        return random.random(self.memory,sample_size)
+        return random.sample(self.memory,sample_size)
     
     #to see the current buffer size
-    def __len(self):
+    def __len__(self):
         return len(self.memory)
