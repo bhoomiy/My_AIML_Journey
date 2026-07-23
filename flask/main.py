98006cf6a21482c7676873ac7cf8ab5c5eab50c0
bhoomiy <bhoomiyadav039@gmail.com>
2026-07-23 23:26:42 +0530

    half done

--- flask/main.py
+++ flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__) #the instance of the app we are building
 
@@ -7,10 +7,16 @@ app = Flask(__name__) #the instance of the app we are building
 def hello_world():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login",methods=["GET","POST"])
 def login():
-    return render_template("login.html")
-
+    if request.method=="POST":
+            print(request.form)
+            name=request.form["username"]
+            password=request.form["password"]
+    
+            return f"<p>Welcome {name}!!!</p>"
+    else:#get request hai
+        return render_template("login.html")
 
 if __name__=='__main__': #it means it will only run when executed via command line interface and not when this file is imported to other file
     app.run(debug=True)
